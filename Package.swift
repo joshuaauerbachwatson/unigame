f7d8fa53dede40963fb40437610ca052ce26cf4d
Josh Auerbach <joshauerbachwatson@gmail.com>
2024-12-02 16:18:16 -0500

    Add optional-use Auth0 token provider
    Also add Auth0 dependency

--- Package.swift
+++ Package.swift
@@ -15,14 +15,17 @@ let package = Package(
             targets: ["unigame"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/joshuaauerbachwatson/AuerbachLook.git", branch: "main") ],
+        .package(url: "https://github.com/joshuaauerbachwatson/AuerbachLook.git", branch: "main"),
+        .package(url: "https://github.com/auth0/Auth0.swift", .upToNextMajor(from: "2.10.0")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "unigame",
             dependencies: [
-                .product(name: "AuerbachLook", package: "auerbachlook")
+                .product(name: "AuerbachLook", package: "auerbachlook"),
+                .product(name: "Auth0", package: "Auth0.swift")
             ]),
     ]
 )
---
+++ Sources/unigame/Auth0TokenProvider.swift
@@ -0,0 +1,25 @@
+//
+//  CredentialStore.swift
+//  anyCards
+//
+//  Created by Josh Auerbach on 4/24/24.
+//
+
+import Foundation
+import Auth0
+import AuerbachLook
+
+// An Auth0 implementation of unigame TokenProvider.  An app that wishes to use this TokenProvider must obtain
+// an Auth0 application which aligns with the bundle id of the app.  Then, an Auth0.plist file must
+// be available as a resource, containing a clientId and domain matching the Auth0 application.
+final class Auth0TokenProvider: TokenProvider {
+    func login() async -> Result<Credentials, Error> {
+        do {
+            let auth0creds = try await Auth0.webAuth().useHTTPS().audience("https://unigame.com").start()
+            let credentials = Credentials(accessToken: auth0creds.accessToken, expires: auth0creds.expiresIn)
+            return .success(credentials)
+        } catch {
+            return .failure(error)
+        }
+    }
+}
