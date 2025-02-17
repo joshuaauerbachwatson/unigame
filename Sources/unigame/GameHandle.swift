5c8a7bcc64cfe0b671f60c2a2c33534a213d3d0b
Josh Auerbach <joshauerbachwatson@gmail.com>
2025-02-17 11:00:12 -0500

    Clean up some comment line lengths

--- Sources/unigame/GameHandle.swift
+++ Sources/unigame/GameHandle.swift
@@ -31,9 +31,10 @@ public protocol GameHandle {
     // Called when another player has transmitted new state.
     func stateChanged(_ data: [UInt8]) -> Error?
 
-    // Called in order to obtain the current state of the game for transmission, either during setup or during play.
-    // Note that the duringSetup argument can only be true for lead players since other players do not experience a
-    // setup phase.  The flag is a hint to avoid sending things over and over that will not have changed after setup.
+    // Called in order to obtain the current state of the game for transmission, either
+    // during setup or during play.  Note that the duringSetup argument can only be true for
+    // lead players since other players do not experience a setup phase.  The flag is a hint
+    // to avoid sending things over and over that will not have changed after setup.
     func encodeState(duringSetup: Bool) -> [UInt8]
     
     // The SwiftUI view to use as the main subview during setup.  If nil, there id no setup phase.
@@ -42,10 +43,11 @@ public protocol GameHandle {
     // The SwiftUI view to use as the main subview during play
     var playingView: any View { get }
 
-    // The appId.  When using the server, this is prepended to the game token.  When using MultiPeer, this becomes
-    // the serviceType, so that game tokens are interpreted only within the scope of a single app.  In both cases it
-    // cuts down on the likelihood of collision between unrelated grouops.  But, because it is also the official
-    // MultiPeer service type, it must be declared in InfoPlist:
+    // The appId.  When using the server, this is prepended to the game token.
+    // When using MultiPeer, this becomes the serviceType, so that game tokens
+    // are interpreted only within the scope of a single app.  In both cases it
+    // cuts down on the likelihood of collision between unrelated groups.
+    // But, because it is also the official MultiPeer service type, it must be declared in InfoPlist:
     //     <key>NSLocalNetworkUsageDescription</key>
     //     <string>Communicate with other players nearby</string>
     //     <key>NSBonjourServices</key>
@@ -56,9 +58,8 @@ public protocol GameHandle {
     var appId: String { get }
 }
 
-// A stand-in for the real token provider, allowing a UnigameModel to be instantiated in previews, etc.
-// This does _not_ support communication with the server.
-// A Dummy GameHandle allowing UnigameModel to be instantiated in previews, etc.  There is no real game logic
+// A Dummy GameHandle allowing UnigameModel to be instantiated in previews, etc.
+// There is no real game logic.
 struct DummyGameHandle: GameHandle {
     var tokenProvider: (any TokenProvider)? = nil
     var numPlayerRange: ClosedRange<Int> = 1...6
